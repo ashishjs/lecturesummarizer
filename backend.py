e47f190de7c13d931a090509c7bbd0e8315c5570
Jared Rickert <jaredrickert52@gmail.com>
2017-09-16 23:03:24 -0500

    added error handling to backend

--- backend.py
+++ backend.py
@@ -2,21 +2,26 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from gensim.summarization import summarize, keywords
 
-
 app = Flask(__name__)
 api = Api(app)
 
 
 @app.route('/api/summarize/', methods=['POST', 'GET'])
 def Summarize():
-    data = request.json.get('data')
-    response = {}
+    response = {
+        'result': {}
+    }
+
     try:
-        response['result'] = {}
+        data = request.json.get('data')
         response['result']['summary'] = summarize(data, ratio=0.20)
         response['result']['keywords'] = keywords(data, ratio=0.01).split('\n')
     except ValueError as e:
         response['result'] = str(e)
+
+    except AttributeError as e:
+        response['result'] = "Missing Text"
+
     return jsonify(response)
 
 
